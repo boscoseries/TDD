641d93c1dafc0372e061fdf9a2602e26aa3d51b5
Johnbosco <boscoseries@gmail.com>
2019-04-06 13:09:36 +0100

    update test cases for function that will add infinite number of arguments to 7

--- addAllNumbers/addAllNumbers.test.js
+++ addAllNumbers/addAllNumbers.test.js
@@ -1,23 +1,27 @@
 var addAllNumbers = require('./addAllNumbersSolution');
 
-describe("Add all numbers", function () {
+describe("Add all numbers in the input", function() {
 
-  test("Expect 12 and 1 to return 13", function () {
-    expect(addAllNumbers(12, 1).toEqual(13));
+  test("Expect 12 and 1 to return 13", function() {
+    expect(addAllNumbers(12, 1)).toEqual(13);
   });
-  test("Expect 2, 3, 5, 5, 12, 6 and 8 to return 41", function () {
-    expect(addAllNumbers(2, 3, 5).toEqual(41));
+  test("Expect 2, 3, 5, 5, 12, 6 and 8 to return 41", function() {
+    expect(addAllNumbers(2, 3, 5, 5, 12, 6, 8)).toEqual(41);
   });
-  test("Expect 7, -2, 0, -3 and 5 to return 7", function () {
-    expect(addAllNumbers(7, -2, 0, -3, 5).toEqual(7))
+  test("Expect 7, -2, 0, -3 and 5 to return 7", function() {
+    expect(addAllNumbers(7, -2, 0, -3, 5)).toEqual(7)
   });
-  test("Expect 'NaN input(s)', to return NaN", function () {
-    expect(addAllNumbers(7, 2, '', -3, 5).toBeNaN());
+  test("Expect 'all NaN input(s)', to return NaN", function() {
+    expect(addAllNumbers('7', '2', '', '-3', '5')).toBeNaN();
   });
-  test("Expect 0.1 and 0.2 to return 0.30", function () {
-    expect(addAllNumbers(0.1, 0.2).toEqual(0.30));
+  test("Expect 1, 2, 3, 1, 's', true, and 10 to return 17", function() {
+    expect(addAllNumbers(1, 2, 3, 1, 's', true, 10)).toEqual(17);
   });
-  test("Expect 1.23, 0.126, 19 and 0.009 to return 20.37", function () {
-    expect(addAllNumbers(1.23, 0.126, 19, 0.009).toEqual(20.37));
+  test("Expect 0.1 and 0.2 to return 0.30", function() {
+    expect(addAllNumbers(0.1, 0.2)).toEqual(0.30);
   });
-})
+  test("Expect 1.23, 0.126, 19 and 0.009 to return 20.37", function() {
+    expect(addAllNumbers(1.23, 0.126, 19, 0.009)).toEqual(20.37);
+  });
+
+});
