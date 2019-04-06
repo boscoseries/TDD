bfacae85d05fec0711936877724165641e4932bb
Johnbosco <boscoseries@gmail.com>
2019-04-06 13:05:10 +0100

    refactor addTwoNumbers function to pass tests for boolean inputs

--- addTwoNumbers/addTwoNumbersSolution.js
+++ addTwoNumbers/addTwoNumbersSolution.js
@@ -1,10 +1,7 @@
 module.exports = function addTwoNumbers(x, y) {
 
-if ( Number.isNaN(y / x) ) {
-  return NaN
-} else if
-( x%1 != 0 || y%1 != 0 ) {
-    return Number((x + y).toFixed(2))
+  if ( typeof x === 'number' && typeof y === 'number' ) {
+    return Number( (x + y).toFixed(2) );
   }
-  return x + y;
-}
+  else return NaN
+  }  
